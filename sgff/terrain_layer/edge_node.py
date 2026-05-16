aa547c1248234bb9a76333cd8d9e28d6e38757a3
Meriem1551 <mm_boussaid@esi.dz>
2026-05-16 13:48:55 +0100

    adding passive redundancy tactic

--- sgff/terrain_layer/edge_node.py
+++ sgff/terrain_layer/edge_node.py
@@ -15,6 +15,7 @@ TOPIC_MQTT   = "sgff/capteurs"
 TOPIC_CRITIQUE = "sensor-critical"
 TOPIC_NORMAL   = "sensor-qualified"
 
+
 # Seuils de détection
 SEUIL_TEMP_CRITIQUE = 70.0
 SEUIL_TEMP_NORMAL   = 50.0
@@ -29,6 +30,10 @@ HEARTBEAT_TIMEOUT = 15  # secondes
 
 KAFKA_BOOTSTRAP = os.getenv("KAFKA_BROKER", "localhost:9092")
 
+# config node
+node_mode = "CENTRAL"
+last_seen_central = time.time()
+CENTRAL_TIMOUT = 30 
 # --- Fonctions de Connexion Résilientes ---
 
 def get_kafka_producer(config):
@@ -70,8 +75,28 @@ def classifier_message(mesure):
         return "NORMAL"
     return "ROUTINE"
 
+def delivary_callback(err, msg):
+    global last_seen_central
+    if err is None:
+        last_seen_central = time.time()  # Mise à jour du heartbeat sur succès de livraison
+    else:
+        print(f"[KAFKA] Erreur de livraison : {err.str()}")
+
+# Tactique : passive redundancy (P2P fallback)
+def activer_mode_P2P():
+    global node_mode
+    node_mode = "P2P"
+    print("[P2P] ⚠️  Centrale perdue → Mode P2P activé")
+    
+def desactiver_mode_P2P():
+    global node_mode, last_seen_central
+    node_mode = "CENTRAL"
+    last_seen_central = time.time()
+    print("[P2P] ✅ Centrale retrouvée → Mode CENTRAL réactivé")
+
 def on_message(client, userdata, msg):
     """Traitement des messages MQTT reçus."""
+    global last_seen_central
     try:
         mesure = json.loads(msg.payload.decode("utf-8"))
         capteur_id = mesure.get("capteur_id", "?")
@@ -94,23 +119,38 @@ def on_message(client, userdata, msg):
         mesure["niveau"] = niveau
         topic = TOPIC_CRITIQUE if niveau == "CRITIQUE" else TOPIC_NORMAL
         
-        producer.produce(topic, json.dumps(mesure).encode("utf-8"))
-        producer.poll(0) # Déclenche les callbacks internes
-
-        symbole = "🔴" if niveau == "CRITIQUE" else "🟡"
-        print(f"[EDGE] {symbole} Envoyé vers {topic} | {capteur_id} ({niveau})")
+        if node_mode == "CENTRAL":
+            producer.produce(topic, json.dumps(mesure).encode("utf-8"), callback=delivary_callback)
+            producer.poll(0) # Déclenche les callbacks internes
+            
+            symbole = "🔴" if niveau == "CRITIQUE" else "🟡"
+            print(f"[EDGE] {symbole} Envoyé vers {topic} | {capteur_id} ({niveau})")
+        else:
+            symbole = "🔴" if niveau == "CRITIQUE" else "🟡"
+            print(f"[P2P] {symbole} traitement local | {capteur_id} ({niveau})")   
 
     except Exception as e:
         print(f"[EDGE] Erreur : {e}")
 
+
 def surveiller_heartbeat():
     """Tactique : Heartbeat."""
+    global last_seen_central, node_mode
     while True:
         time.sleep(5)
         maintenant = time.time()
         for cid, last in list(derniere_activite.items()):
             if maintenant - last > HEARTBEAT_TIMEOUT:
                 print(f"[HEARTBEAT] ⚠️ Capteur {cid} déconnecté !")
+        silence = maintenant - last_seen_central
+        if node_mode == "CENTRAL" and silence > CENTRAL_TIMOUT:
+            activer_mode_P2P()
+        elif node_mode == "P2P":
+            try:
+                producer.list_topics(timeout=2.0)  # Vérifie si Kafka est de nouveau accessible
+                desactiver_mode_P2P()
+            except:
+                pass  # Toujours en mode P2P
 
 # --- Lancement ---
 
